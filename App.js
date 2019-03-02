883e2838e9d16ba8c3c984f6c8ecb8789ad61d50
thor-jc <jc@alloatech.com>
2019-03-02 11:53:34 -0500

    working on packaging this module to be consumed in another

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class AuthApp extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your KICKASS APP!</Text>
+        <Text>Auth</Text>
       </View>
     );
   }
@@ -14,7 +14,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#22235630',
     alignItems: 'center',
     justifyContent: 'center',
   },
